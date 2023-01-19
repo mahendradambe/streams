bb7564cb19a8a931589b6a3ca512707296864b61
mahendradambe <45140148+mahendradambe@users.noreply.github.com>
2023-01-19 10:56:35 +0530

    refactor: cleanup

--- src/__tests__/helpers/models.ts
+++ src/__tests__/helpers/models.ts
@@ -41,52 +41,6 @@ export class User {
         this.id = id;
     }
 
-    // createPost(content: string = "") {
-    //     const post: Post = new Post({ content, creator: this.id });
-
-    //     this.posts.push(post.id);
-    //     POSTS.push(post);
-
-    //     userFeedManager.add(
-    //         this.id,
-    //         new Activity(Date.now(), this.toJson(), Create, post.toJson())
-    //     );
-
-    //     return post;
-    // }
-
-    // likePost(postId: ObjectId) {
-    //     const post = POSTS.find((p) => p.id === postId);
-
-    //     if (post) {
-    //         this.liked.push(postId);
-    //         post.like(this.id);
-
-    //         userFeedManager.add(
-    //             this.id,
-    //             new Activity(Date.now(), { id: this.id }, Like, {
-    //                 id: post.id,
-    //             })
-    //         );
-    //     }
-    // }
-
-    // dislikePost(postId: ObjectId) {
-    //     const post = POSTS.find((p) => p.id === postId);
-
-    //     if (post) {
-    //         const postIdx = this.liked.indexOf(postId);
-    //         this.liked.splice(postIdx, 1);
-
-    //         post.dislike(this.id);
-
-    //         userFeedManager.add(
-    //             this.id,
-    //             new Activity(Date.now(), this.toJson(), Dislike, post.toJson())
-    //         );
-    //     }
-    // }
-
     toJSON() {
         return {
             id: this.id,
